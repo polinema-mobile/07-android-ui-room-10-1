8947bc87631c6b575c93ce2ca392d9503c4a65e1
Fenti08 <58581075+Fenti08@users.noreply.github.com>
2020-09-30 14:04:13 +0700

    mengrim data

--- app/src/main/java/com/example/activitydanintent/MainActivity.java
+++ app/src/main/java/com/example/activitydanintent/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.activitydanintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.DatePicker;
 
@@ -15,18 +18,26 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     DatePickerDialog picker;
-    EditText edtTanggalLahir;
+    EditText edit_tanggal_lahir, edit_nama, edit_nim;
     Button btnGet;
     Spinner spinner;
+    RadioGroup chosen;
+    RadioButton rdbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //1 findviewbyid btnGet = ...
-        edtTanggalLahir=(EditText) findViewById(R.id.edtTanggalLahir);
-        edtTanggalLahir.setInputType(InputType.TYPE_NULL);
-        edtTanggalLahir.setOnClickListener(new View.OnClickListener(){
+        btnGet=(Button)findViewById(R.id.button);
+        edit_nama=(EditText)findViewById(R.id.edtNama);
+        edit_nim=(EditText)findViewById(R.id.edtNim);
+        edit_tanggal_lahir=(EditText) findViewById(R.id.edtTanggalLahir);
+        edit_tanggal_lahir.setInputType(InputType.TYPE_NULL);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        chosen=(RadioGroup)findViewById(R.id.rdGroup1);
 
+        edit_tanggal_lahir.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 final Calendar cldr = Calendar.getInstance();
@@ -38,17 +49,34 @@ public class MainActivity extends AppCompatActivity {
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                edtTanggalLahir.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                edit_tanggal_lahir.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
                             }
                         }, year, month, day);
                 picker.show();
             }
         });
-
         //2 click listener untuk button get
-        // 3 di dalam click listener ambil value nya edit text
-        // 4 buat intent
-        // 5 isi variabel intent (bundle)
-        // 6 send intent ke detail activity
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String name = edit_nama.getText().toString();
+                String nim = edit_nim.getText().toString();
+                String tggl = edit_tanggal_lahir.getText().toString();
+                String chosenOption = spinner.getSelectedItem().toString();
+                int radiogrp = chosen.getCheckedRadioButtonId();
+                rdbtn = (RadioButton)findViewById(radiogrp);
+                String gender = rdbtn.getText().toString();
+
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("NAME",name);
+                intent.putExtra("NIM",nim);
+                intent.putExtra("TTL",tggl);
+                intent.putExtra("CHOSEN_OPTION", chosenOption);
+                intent.putExtra("GENDER", gender);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/activitydanintent/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.activitydanintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        //1 findViewById 5 element id
+        TextView namaMhs = (TextView) findViewById(R.id.Nama_mahasiswa);
+        TextView nimMhs = (TextView) findViewById(R.id.nim);
+        TextView ttl = (TextView) findViewById(R.id.tanggal_lahir);
+        TextView gender = (TextView) findViewById(R.id.kelamin);
+        TextView jurusan = (TextView)findViewById(R.id.jurusan);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("NAME");
+        String nim = intent.getStringExtra("NIM");
+        String tggl = intent.getStringExtra("TTL");
+        String jnskelamin = intent.getStringExtra("GENDER");
+        String chosen = intent.getStringExtra("CHOSEN_OPTION");
+
+        namaMhs.setText("NAMA : "+name);
+        nimMhs.setText("NIM   : "+nim);
+        ttl.setText("TTL   : "+tggl);
+        gender.setText("GENDER   : "+jnskelamin);
+        jurusan.setText("JURUSAN   : "+chosen);
+
+    }
+}
